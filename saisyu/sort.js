61a28b2b1e8565c604ebc8d49f74c9659efc3c2e
xl3lackout <xl3lackout.flux@gmail.com>
2022-03-30 01:13:05 -0400

    Gathering, Skills, Caravan, Raviente Translations.

--- saisyu/sort.js
+++ saisyu/sort.js
@@ -16,9 +16,9 @@ var addEvent = function (elm, type, func) {
 var topDiv=document.getElementsByTagName("div")[0],i=document.createElement("input");
 i.type="button";
 //昼夜
-i.value="昼",i.title="昼で絞込み",i.style.marginLeft = "20px";
+i.value="Day",i.title="昼で絞込み",i.style.marginLeft = "20px";
 topDiv.appendChild(i.cloneNode(false));
-i.value="夜",i.title="夜で絞込み",i.style.marginLeft = "0";
+i.value="Night",i.title="夜で絞込み",i.style.marginLeft = "0";
 topDiv.appendChild(i.cloneNode(false));
 topDiv=i=null;
 
@@ -30,7 +30,7 @@ function (evt) {
 	var t = evt.target;
 	/*@end@*/
 	if (t.tagName === "INPUT"){
-		var tr=document.getElementsByTagName("tbody")[0].rows, N=tr.length,wk = t.value === "夜" ? "y" : "";
+		var tr=document.getElementsByTagName("tbody")[0].rows, N=tr.length,wk = t.value === "Night" ? "y" : "";
 		for(var i=0; i<N; i++) tr[i].style.display = tr[i].className === wk || tr[i].cells[1]./*@if (@_jscript_version < 9) innerText @else@*/ textContent /*@end@*/ === "0" ? "" : "none";
 	}
 });
